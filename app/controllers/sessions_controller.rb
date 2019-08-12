6b0cdabf84e33efb78f7bf849e10506e1326c13f
Jasper Tabacug <tabacugjasper@gmail.com>
2019-08-12 17:30:05 +0800

    Add user Authenication. Created error and success messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,20 @@ class SessionsController <ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "You have successfully logged in"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your login information!"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'chatroom#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, except:[:new]
+
+  resources :messages
+
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
