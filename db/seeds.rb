64f399fd9af8f0de1251e7184e7947fd73602d9f
Jasper Tabacug <tabacugjasper@gmail.com>
2019-08-12 15:41:04 +0800

    Build User & Message Resource, also their seed. Installed: Bcrypt. Faker and Hirb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  name = Faker::JapaneseMedia::OnePiece.unique.character
+  puts "User: #{name}"
+  User.create(username: name,
+              password: "password")
+end
+
+15.times do
+  Message.create(body: Faker::JapaneseMedia::OnePiece.quote,
+                 user_id: Faker::Number.within(range: 1..5))
+end
