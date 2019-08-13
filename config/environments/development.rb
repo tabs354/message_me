d927787672f88dbc94a8a9fecb8fa7164036e534
Jasper Tabacug <tabacugjasper@gmail.com>
2019-08-13 14:47:02 +0800

    Modify development.rb, set action cable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # config.action_cable.disable_request_forgery_protection = true
-  # config.action_cable.disable_request_origins = ['https://']
+  config.action_cable.disable_request_origins = ['https://tabs-chatroom.herokuapp.com']
 end
